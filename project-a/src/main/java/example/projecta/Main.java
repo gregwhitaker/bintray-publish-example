b03adaa7562815c1ad059dec436c0f03fb1c7da5
gregwhitaker <gwhitake@gmail.com>
2018-12-27 14:38:59 -0800

    Doing work

--- project-a/src/main/java/example/projecta/Main.java
+++ project-a/src/main/java/example/projecta/Main.java
@@ -1,10 +1,25 @@
 package example.projecta;
 
 import example.common.MessagePrinter;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String... args) {
-        MessagePrinter.print("Project A!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Component
+    public class AppStartupRunner implements ApplicationRunner {
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            MessagePrinter.print("Started Project A!");
+        }
     }
 }
